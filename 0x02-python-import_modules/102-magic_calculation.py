3438049f231c6749bb04a6a8473de4359afafa23
obusandy <obungasandrah@gmail.com>
2023-11-02 07:33:42 -0700

    ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,8 +6,8 @@ def magic_calculation(a, b):
 
     if a < b:
         c = add(a, b)
-        for num in range(4, 6):
-            c = add(c, num)
+        for i in range(4, 6):
+            c = add(c, i)
         return (c)
 
     else:
