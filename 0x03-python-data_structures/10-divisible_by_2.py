e295d8743446ce8f5308c7b53c9078468f1cae0d
obusandy <obungasandrah@gmail.com>
2023-11-04 06:59:20 -0700

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def divisible_by_2(my_list=[]):
-    i = [not num % 2 for num in my_list]
+def divisible_by_2(my_lst=[]):
+    i = [not num % 2 for num in my_lst]
     return (i)
