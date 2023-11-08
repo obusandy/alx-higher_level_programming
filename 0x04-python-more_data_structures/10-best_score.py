aa0bc96a65ae36f7d73ddfd8e7bd2d99b3f6cb98
obusandy <obungasandrah@gmail.com>
2023-11-08 07:49:10 -0800

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-
+def best_score(a_dict):
+    if not a_dict:
+        return None
+    curr_best = max(a_dict.values())
+    for key, numbr in a_dict.items():
+        if numbr == curr_best:
+            bst_key = key
+    return bst_key
