803c746dd74a9f699324f86597403485e214518c
obusandy <obungasandrah@gmail.com>
2023-11-08 05:51:33 -0800

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+
+    otput_list = my_list.copy()
+    for x, elmnt in enumerate(otput_list):
+        if elmnt == search:
+            otput_list[x] = replace
+
+    return otput_list
