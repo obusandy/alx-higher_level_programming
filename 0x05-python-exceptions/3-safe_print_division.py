625b17794ac835312f28e02d70071a9c37f3b091
obusandy <obungasandrah@gmail.com>
2023-11-20 07:26:14 -0800

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    divisn_rslt = None
     try:
         divisn_rslt = a / b
     except ZeroDivisionError:
-        pass
+        divisn_rslt = None
     finally:
         print("Inside divisn rslt: {}".format(divisn_rslt))
     return divisn_rslt
