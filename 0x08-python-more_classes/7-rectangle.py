b57858dd7955c0eac09522cb431738f42b04d605
obusandy <obungasandrah@gmail.com>
2023-11-27 13:00:31 -0800

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
         """ assigns the value of the width
         Handles: Type and value error
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -48,7 +48,7 @@ class Rectangle:
         """ assigns the value of the width
         Handles: TH type an value errors
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
