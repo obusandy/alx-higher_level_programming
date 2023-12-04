6330f914a3ace5e452419ee6c34d3065548d9411
obusandy <obungasandrah@gmail.com>
2023-12-04 08:35:04 -0800

    exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Does a checks to determine if object is an instance of a class"""
 
-
 def is_same_class(obj, a_class):
     """
     Args: an object and a class to be compared to
