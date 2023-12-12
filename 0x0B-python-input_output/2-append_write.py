a176b2f4ce44bd18ffdbb5e357a13811c6ebdf7f
obusandy <obungasandrah@gmail.com>
2023-12-12 02:38:48 -0800

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module: write_file
+
+Contains a function that writes content to a text file.
+"""
+
+def write_file(file_name="", txt=""):
+    """
+    Writes content to a specific text file.
+
+    Arguments:
+        file_path (str): The path to the file.
+        txt (str): The text to be written on the file.
+
+    Raises:
+        Exception: For general errors while opening or writing to the file.
+    Returns: 
+        no of characters
+    """
+    with open(file_name, 'a', encoding="utf-8") as f:
+        return f.write(txt)
+~                            
