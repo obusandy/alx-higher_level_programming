3ec8d5c89bc2a8c080efcfd0c985ca3e7ce872c9
obusandy <obungasandrah@gmail.com>
2023-12-12 01:57:13 -0800

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 
+Module: file_reader
+
+which Contains a function that reads content from a file.
+"""
+
+def read_file(file_name=""):
+    """ 
+    Reads the content from a specific file.
+
+    Arguments:
+        file_name (str): The path to the file.
+
+    Raises:
+        FileNotFoundErr: If the file is not found, read or opened.
+    """
+
+        with open(file_name, 'r', encoding="utf-8") as fle:
+        readtxt_data = fle.read()
+        print(readtxt_data, end='')
