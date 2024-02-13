b9927a44f59592cee3c6ba81a12c546a4afbd903
obusandy <obungasandrah@gmail.com>
2024-02-13 02:58:45 -0800

    JavaScript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a class Rectangle that defines a rectangle:
+// using the class notation for defining the class
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
