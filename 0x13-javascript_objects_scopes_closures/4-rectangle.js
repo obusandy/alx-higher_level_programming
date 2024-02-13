67fb1d3b2ada6c24c091f1e14bc85c977b0a99cc
obusandy <obungasandrah@gmail.com>
2024-02-13 03:36:49 -0800

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 /**
- * a class Rectangle that defines a rectangle:
+ * a class Rectangle that defines a rectangle
  * The constructor must take 2 arguments: w and h
  */
+
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
