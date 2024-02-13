368e5d28494f854625766eb36827456fcce60fe8
obusandy <obungasandrah@gmail.com>
2024-02-13 00:28:40 -0800

    my commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a script that:
+// prints 3 lines using an array of string and loop
+
+const row = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let j = 0; j < row.length; j++)
+{
+  console.log(row[j]);
+}
