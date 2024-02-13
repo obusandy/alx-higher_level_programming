7e7f83ae3f45b2b4d9d190778d68474bbcc70449
obusandy <obungasandrah@gmail.com>
2024-02-13 00:38:28 -0800

    my commit

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that prints x times “C is fun”
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const y = Number(process.argv[2]);
+  let j = 0;
+  while (j < y) {
+    console.log('C is fun');
+    j++;
+  }
+}
