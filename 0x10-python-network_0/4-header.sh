e9447b0ece0598d3abbc8d8244e115ed056981a1
obusandy <obungasandrah@gmail.com>
2024-02-29 04:49:20 -0800

    header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# this is a script that takes in a url as an arg
-# sends a GET request to the URL, and displays the body of the response
+# this is a script that takes in a url as an arg and sends a get request
 curl -sH "X-School-User-Id: 98" "$1"
