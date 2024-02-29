64eae95afe831d09e9c81b35ba39aa5e48bce74e
obusandy <obungasandrah@gmail.com>
2024-02-29 04:34:34 -0800

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a GET request
-# to the URL, and displays the body of the response
+# a Bash script that takes in a URL, sends a GET request and displays
 curl -sfL "$1"
